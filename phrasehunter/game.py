25ab697f6dc14781fdb7b85e30033448e3447bc2
guy-lorshbaugh <67803858+guy-lorshbaugh@users.noreply.github.com>
2020-08-10 15:14:12 -0700

    Step 12 of the walkthrough

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,48 @@
 # Create your Game class logic in here.
+import random
+from phrasehunter.phrase import Phrase
+
+class Game:
+    def __init__(self):
+        self.missed = 0
+        self.phrases = [
+            Phrase("Life is like a box of chocolates"),
+            Phrase("Tomorrow is another day"),
+            Phrase("May the Force be with you"),
+            Phrase("Go away or I shall taunt you a second time"),
+            Phrase("Cant stop here this is bat country"),
+        ]
+
+        self.active_phrase = random.choice(self.phrases)
+        self.guesses = []
+
+    def welcome(self):
+        stars = "*" * 39
+        print(f"""
+        {stars}
+          Welcome to Python Wheel of Fortune!
+              (except without the wheel)     
+        {stars}
+        """)
+
+    def start(self):
+        self.welcome()
+        while self.missed < 5 and self.active_phrase.check_complete(self.guesses) == False:
+            if _ in guesses:
+                print("You've guessed the following lettters:", *self.guesses)    
+            print(f"You have used {self.missed} out of 5 guesses.\n")
+            self.active_phrase.display(self.guesses)
+            self.user_guess = self.get_guess()
+            self.guesses.append(self.user_guess)
+            if not self.active_phrase.check_guess(self.user_guess):
+                self.missed += 1
+                print(f"\nSorry, '{self.user_guess}' is not in the phrase!\n")
+            
+        print("Bummer!  You've used all your guesses.\n")
+
+
+    def get_guess(self):
+        self.guess = input("Guess a letter!  ")
+        return(self.guess)
+
+        #  and self.active_phrase.check_complete(self.guesses) == False
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,33 @@
 # Create your Phrase class logic here.
+
+class Phrase:
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+        
+    def display(self, guesses):
+        self.letters = []
+        self.output = []
+        self.letters.extend(self.phrase)
+        for letter in self.letters:
+            if letter == " ":
+                self.output.append(" ")
+            elif letter in guesses:
+                self.output.append(letter)
+            else:
+                self.output.append("_")
+        print("***  ", *self.output, "  ***\n")
+
+    def check_guess(self, guess):
+        if guess in self.letters:
+            return True
+        else:
+            return False
+
+    def check_complete(self, guesses):
+        self.letters = []
+        self.phrase = self.letters.extend(self.phrase.phrase)
+        # print(phrase)
+        for guess in guesses:
+            if guess not in self.phrase:
+                return False
+            else: return True
